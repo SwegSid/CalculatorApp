81668dbf4f1aaf3496760f00cd151094a397d408
SwegSid <bhap2002@yahoo.com>
2022-08-23 11:44:09 -0500

    worked on addition button and method

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void addition(View view){
+        EditText number1 = findViewById(R.id.number1);
+        EditText number2 = findViewById(R.id.number2);
+        TextView answer = findViewById(R.id.answer);
+
+        int one = Integer.parseInt(number1.getText().toString());
+        int two = Integer.parseInt(number2.getText().toString());
+
+        int sum = one + two;
+
+        answer.setText("" + answer);
     }
 }
